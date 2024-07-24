73ffe133e0e1bbb95d1028235e0ae70ea4b602f2
Nguyen Ai Dan <dan3366881@gmail.com>
2024-07-24 09:20:59 +0700

    Add project files.

---
+++ NguenAiDan_project1/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NguenAiDan_project1
+{
+    public class SchoolContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SchoolDb;Trusted_Connection=True;");
+        }
+    }
+}
